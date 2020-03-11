d9a4f0f879cdffae096e724066503cb8dbb7fca0
gorbv <volodymyr.gorbenko@leonteq.com>
2020-03-11 08:55:02 +0100

    Initial commits

--- rust/algo/src/main.rs
+++ rust/algo/src/main.rs
@@ -1,3 +1,22 @@
+use termion::{color, style};
+
+fn foobar(_x: i32) {
+    let color = color::Fg(color::LightCyan);
+    let x = {
+        let y = 1; // first statement
+        let z = 2; // second statement
+        y + z // this is the *tail* - what the whole block will evaluate to
+    };
+    println!("{}x = {}", color, x);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = vec![1, 2, 3, 4, 5, 6, 7, 8]
+        .iter()
+        .map(|x| x + 3)
+        .fold(0, |x, y| x + y);
+
+    foobar(x); // the type of `x` will be inferred from here
+
+    println!("{}{}Hello, world!", color::Fg(color::LightMagenta), style::Bold);
 }
